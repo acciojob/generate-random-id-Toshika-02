c249669d3f517b9c2785b0579a499482dc149980
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-19 10:42:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function makeid(l) {
   // write your code here
 	let values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
      let str = "";
-	 while(n--){
+	 while(l--){
 		 let anyNumber = parseInt(Math.random() * 35)
 		 str += values[anyNumber]
 	 }
